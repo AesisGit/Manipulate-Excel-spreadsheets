a40abc8b28d46727aca3391ccb0633452948bf52
goranaviani <goranaviani@gmail.com>
2019-01-12 20:28:23 +0100

    loop throgh all rows of the sheet in that specific column which is done with function get_all_values_by_cell_letter. The function will print all values of those cells.

--- Manipulate_Excel_spreadsheets.py
+++ Manipulate_Excel_spreadsheets.py
@@ -20,11 +20,21 @@ def get_column_letter(specificCellLetter):
     print(letter)
     return letter
 
+#not needed as get_all_values_by_cell_list uses to many resources.
 def get_all_cells_from_specific_letter(letter):
     specificCells = []
     for x in range(2, currentSheet.max_row + 1): # 2 is because there is column name on 1
         specificCells.append(letter + str(x))
     return specificCells
+
+
+def get_all_values_by_cell_letter(letter):
+    for row in range(1, currentSheet.max_row + 1):
+        for column in letter:
+            cell_name = "{}{}".format(column, row)
+            #print(cell_name)
+            print("cell position {} has value {}".format(cell_name, currentSheet[cell_name].value))
+
 
 
 for sheet in allSheetNames:
@@ -33,10 +43,18 @@ for sheet in allSheetNames:
     specificCellLetter = (find_specific_cell())
     letter = get_column_letter(specificCellLetter)
 
-    specificCells = get_all_cells_from_specific_letter(letter)
-    print(specificCells)
+    #specificCells = get_all_cells_from_specific_letter(letter)
+    get_all_values_by_cell_letter(letter)
+   # get_all_values_by_cell_list(specificCells)
 
-
+#not needed as it uses unnececary resources to find values. get_all_cells_from_specific_letter is more efficient.
+def get_all_values_by_cell_list(specificCells):
+    for row in range(1, currentSheet.max_row + 1):
+        for column in "ABCDEF":  # Here you can add or reduce the columns
+            cell_name = "{}{}".format(column, row)
+            if cell_name in specificCells:
+                print("cell position {} has value {}".format(cell_name, currentSheet[cell_name].value))
+#not needed.
 def read_all():
     for row in range(1, currentSheet.max_row + 1):
         print(row)
