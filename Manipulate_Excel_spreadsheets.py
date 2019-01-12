8da49e1b70371654bf3e9b879aea6f4063177208
goranaviani <goranaviani@gmail.com>
2019-01-12 19:22:17 +0100

    Reading through cells where the cell range (A,B,C,D,E...) is hardcoded

--- Manipulate_Excel_spreadsheets.py
+++ Manipulate_Excel_spreadsheets.py
@@ -6,7 +6,22 @@ allSheetNames = theFile.sheetnames
 print("All sheet names {} " .format(theFile.sheetnames))
 
 
-for x in allSheetNames:
-    print("Current sheet name is {}" .format(x))
-    currentSheet = theFile[x]
-    print(currentSheet['B4'].value)
+for sheet in allSheetNames:
+    print("Current sheet name is {}" .format(sheet))
+    currentSheet = theFile[sheet]
+    # print(currentSheet['B4'].value)
+
+    #print max numbers of wors and colums for each sheet
+    #print(currentSheet.max_row)
+    #print(currentSheet.max_column)
+
+
+    for row in range(1, currentSheet.max_row + 1):
+        #print(row)
+        for column in "ABCDEF":  # Here you can add or reduce the columns
+
+            cell_name = "{}{}".format(column, row)
+            #print(cell_name)
+            print("cell position {} has  value {}".format(cell_name, currentSheet[cell_name].value))
+
+
