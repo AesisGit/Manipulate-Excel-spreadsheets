86e2f9acce060f5db772530fd97cb0a3a5114b5d
goranaviani <goranaviani@gmail.com>
2019-01-12 20:08:48 +0100

    get_column_letter to get the letter of the column and then loop throgh all rows of the sheet which is done with function get_all_cells_from_specific_letter.

--- Manipulate_Excel_spreadsheets.py
+++ Manipulate_Excel_spreadsheets.py
@@ -6,26 +6,36 @@ allSheetNames = theFile.sheetnames
 print("All sheet names {} " .format(theFile.sheetnames))
 
 
-for sheet in allSheetNames:
-    print("Current sheet name is {}" .format(sheet))
-    currentSheet = theFile[sheet]
-    # print(currentSheet['B4'].value)
-
-    #print max numbers of wors and colums for each sheet
-    #print(currentSheet.max_row)
-    #print(currentSheet.max_column)
-
-    numberOfColumns = currentSheet.max_column + 1;
-
-
 def find_specific_cell():
     for row in range(1, currentSheet.max_row + 1):
         for column in "ABCDEFGHIJKL":  # Here you can add or reduce the columns
             cell_name = "{}{}".format(column, row)
             if currentSheet[cell_name].value == "telephone":
-                print("{1} cell is located on {0}" .format(cell_name, currentSheet[cell_name].value))
+                #print("{1} cell is located on {0}" .format(cell_name, currentSheet[cell_name].value))
                 print("cell position {} has value {}".format(cell_name, currentSheet[cell_name].value))
                 return cell_name
+
+def get_column_letter(specificCellLetter):
+    letter = specificCellLetter[0:-1]
+    print(letter)
+    return letter
+
+def get_all_cells_from_specific_letter(letter):
+    specificCells = []
+    for x in range(2, currentSheet.max_row + 1): # 2 is because there is column name on 1
+        specificCells.append(letter + str(x))
+    return specificCells
+
+
+for sheet in allSheetNames:
+    print("Current sheet name is {}" .format(sheet))
+    currentSheet = theFile[sheet]
+    specificCellLetter = (find_specific_cell())
+    letter = get_column_letter(specificCellLetter)
+
+    specificCells = get_all_cells_from_specific_letter(letter)
+    print(specificCells)
+
 
 def read_all():
     for row in range(1, currentSheet.max_row + 1):
@@ -38,4 +48,3 @@ def read_all():
             print("cell position {} has value {}".format(cell_name, currentSheet[cell_name].value))
 
 
-print(find_specific_cell())
